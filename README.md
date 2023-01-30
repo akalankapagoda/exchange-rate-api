d7a09e1d1779fd12ccbed85260e2a63ac7ca97c0
akalankapagoda <akalankapagoda@gmail.com>
2023-01-30 10:05:03 +0200

    Update API documentation and add CORS.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# exchange-rate-api
+# Exchange Rate API
+
+Exposes a simple API for retrieving exchange rates and converting currency.
+
+## Prerequisites
+
+- Java 17+
+- Maven 2.8+
+
+## How to Build
+
+Navigate to the project root directory and run the following command.
+
+`mvn clean install`
+
+## How to Run
+
+- Update the src/main/resources/application.properties file to adjust configurations if required.
+- Navigate to the project root directory and run the following command.
+  `mvn spring-boot:run`
+
+## How to Verify the Deployment
+
+- Navigate to <base url>/api/exchangeRate/hello
+- This should return an info message with an HTTP SC 200
+
+## API Documentation
+
+This provides a Swagger UI and OpenAPI implementation for documentation.
+
+Please see here for the deployed documentation.
+
+
+
--- src/main/java/com/nosto/exchange/currencyconvert/CurrencyConverterApplication.java
+++ src/main/java/com/nosto/exchange/currencyconvert/CurrencyConverterApplication.java
@@ -2,12 +2,30 @@ package com.nosto.exchange.currencyconvert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CurrencyConverterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+	/**
+	 * Allow CORS so that front-end cal call these APIs.
+	 *
+	 * @return
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
